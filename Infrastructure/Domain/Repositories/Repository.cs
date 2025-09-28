8ef384980f089bc5525d689bb9237622f24b10d5
Pablo B. Martinez Pedroso <pablo.martinezpedroso@ukg.com>
2025-09-28 01:25:19 -0300

    add entity framework and init migration

--- Infrastructure/Domain/Repositories/Repository.cs
+++ Infrastructure/Domain/Repositories/Repository.cs
@@ -1,54 +1,55 @@
 ﻿using Domain.Entities;
 using Domain.IRepositories;
+using Infrastructure.Domain.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Domain.Repositories
 {
     internal class Repository<TEntity> : IRepository<TEntity, Guid> where TEntity : Entity
     {
-        private DbContext DbContext { get; set; }
+        private AppDbContext UnitOfWork { get; set; }
 
-        public Repository(DbContext dbContext)
+        public Repository(AppDbContext dbContext)
         {
-            DbContext = dbContext;
+            UnitOfWork = dbContext;
         }
 
         public TEntity? Get(Guid id, List<string>? includes)
         {
-            return DbContext.Set<TEntity>().FirstOrDefault(e => e.Id == id);
+            return UnitOfWork.Set<TEntity>().FirstOrDefault(e => e.Id == id);
         }
 
         public async Task<IQueryable<TEntity>> GetAllAsync(List<string>? includes, Dictionary<string, bool>? order, CancellationToken cancellationToken)
         {
-            return await Task.FromResult(DbContext.Set<TEntity>());
+            return await Task.FromResult(UnitOfWork.Set<TEntity>());
         }
 
         public Task<TEntity?> GetAsync(Guid id, List<string>? includes = default, CancellationToken cancellationToken = default)
         {
-            return DbContext.Set<TEntity>().FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+            return UnitOfWork.Set<TEntity>().FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
         }
 
         public async Task AddAsync(TEntity item, CancellationToken cancellationToken)
         {
-            await DbContext.Set<TEntity>().AddAsync(item, cancellationToken);
+            await UnitOfWork.Set<TEntity>().AddAsync(item, cancellationToken);
         }
 
         public async Task UpdateAsync(TEntity item, CancellationToken cancellationToken)
         {
             var itemToUpdate = await GetAsync(item.Id, cancellationToken: cancellationToken) ?? throw new ArgumentException("Item to update must be valid and present in database");
-            DbContext.Entry(itemToUpdate).CurrentValues.SetValues(item);
+            UnitOfWork.Entry(itemToUpdate).CurrentValues.SetValues(item);
             await Task.CompletedTask;
         }
 
         public async Task DeleteAsync(TEntity item, CancellationToken cancellationToken)
         {
-            DbContext.Set<TEntity>().Remove(item);
+            UnitOfWork.Set<TEntity>().Remove(item);
             await Task.CompletedTask;
         }
 
         public async Task<int> CommitChangesAsync()
         {
-            return await DbContext.SaveChangesAsync();
+            return await UnitOfWork.SaveChangesAsync();
         }
 
     }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,13 @@
+using Infrastructure.Domain.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
