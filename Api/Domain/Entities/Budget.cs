3bda1143519757be1b001a7b10816c512d4d96e9
Pablo B. Martinez Pedroso <pablo.martinezpedroso@ukg.com>
2025-09-28 22:28:50 -0300

    #2 add entities and migration

---
+++ Api/Domain/Entities/Budget.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Budget : Entity
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Amount must be a positive value.")]
+        public decimal Amount { get; set; }
+
+        public ICollection<Expense> Expenses { get; set; } = [];
+    }
+}
